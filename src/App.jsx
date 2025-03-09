bcba54bc3bd1b0557480e666c2ed1a54da3daf86
AnabiaMansoor <anabia.mansoor98@gmail.com>
2025-03-09 07:23:37 +0500

    Fixed Aboutus import issue

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,13 @@ import './App.css'
 import Navbar from './components/Navbar'
 import HeroSection from "./components/HeroSection";
 import StatsSection from "./components/StatsSection";
-import AboutUs from "./components/Aboutus";
 import Achievements from "./components/Achievements";
 import Capabilities from "./components/Capabilities";
 import BannerSec from "./components/BannerSection";
 import Stories from "./components/Stories";
 import Brands from './components/Brands';
 import Footer from "./components/Footer";
-import AboutUs from "./components/Aboutus";
+import AboutUs from "./components/AboutUs";
 
 function App() {
 
